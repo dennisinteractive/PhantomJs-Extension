4cf558d6dd8142fd00bf7b12217ced9bd40f59aa
David Grayston <david_grayston@dennis.co.uk>
2017-12-07 14:56:04 +0000

    Case 29096; outputting message when CURL request times out

--- src/Behat/PhantomJsExtension/Service/CurlService.php
+++ src/Behat/PhantomJsExtension/Service/CurlService.php
@@ -2,17 +2,18 @@
 namespace Behat\PhantomJsExtension\Service;
 
 use WebDriver\Service\CurlService as WebDriverCurlService;
+use Symfony\Component\Console\Formatter\OutputFormatter;
 
 class CurlService extends WebDriverCurlService {
   /**
    * @var int
    */
-  protected $timeout = 60;
+  protected $timeout = 120;
 
   /**
    * @var int
    */
-  protected $connectTimeout = 60;
+  protected $connectTimeout = 120;
 
   /**
    * {@inheritdoc}
@@ -25,7 +26,17 @@ class CurlService extends WebDriverCurlService {
         CURLOPT_TIMEOUT => $this->timeout,
       )
     );
-    return parent::execute($requestMethod, $url, $parameters, $extraOptions);
+
+    $result = parent::execute($requestMethod, $url, $parameters, $extraOptions);
+
+    // Output message to indicate that the page request reached the timeout.
+    if ((int) $result[1]['total_time'] >= $this->timeout) {
+      $formatter = new OutputFormatter(true);
+      echo $formatter->format("\n<comment>Timed out after " . $this->timeout . " seconds\nURL: " . $url . "\nParameters:\n" . print_r($parameters, TRUE) . "</comment>\n");
+      ob_flush();
+    }
+
+    return $result;
   }
 
   /**
